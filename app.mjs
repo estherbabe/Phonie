bccc95553866622a7e540631ba5bdd6c55ddf0ca
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-07-27 11:20:03 +0100

    .

--- app.mjs
+++ app.mjs
@@ -1,4 +1,5 @@
-var mtn = [
+//====== telecom prefixes ======
+const mtn = [
   "0803",
   "0806",
   "0703",
@@ -10,11 +11,21 @@ var mtn = [
   "0814",
   "0903",
 ];
-var airtel = ["0802", "0808", "0708", "0812", "0701", "0902"];
-var glo = ["0805", "0807", "0705", "0815", "0811", "0905"];
-var etisalat = ["0809", "0818", "0817", "0909"];
+const airtel = ["0802", "0808", "0708", "0812", "0701", "0902"];
+const glo = ["0805", "0807", "0705", "0815", "0811", "0905"];
+const etisalat = ["0809", "0818", "0817", "0909"];
+
+//====== input ======
+const output = document.getElementById("output");
+
+//======== telecom logos ========
+const mtnlogo = document.getElementsByClassName("mtn");
+const airtellogo = document.getElementsByClassName("airtel");
+const glologo = document.getElementsByClassName("glo");
+const ninemobile = document.getElementsByClassName("mobile");
 
 function startApp() {
+  mtn.forEach((string) => {});
   // Your entire app should not necessarily be coded inside this
   // single function (though there's no penalty for that),
   // so create and use/call additional functions from here
