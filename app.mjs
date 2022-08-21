d7ac580aeb43987dd52314862fd712fef701e493
Esther Nwachukwu <estynwachukwu2016@gmail.com>
2022-08-21 12:02:38 +0100

    my first push for javascript

--- app.mjs
+++ app.mjs
@@ -5,7 +5,6 @@ const mtn = [
   "0703",
   "0706",
   "0813",
-  "0813",
   "0816",
   "0810",
   "0814",
@@ -15,23 +14,35 @@ const airtel = ["0802", "0808", "0708", "0812", "0701", "0902"];
 const glo = ["0805", "0807", "0705", "0815", "0811", "0905"];
 const etisalat = ["0809", "0818", "0817", "0909"];
 
-//====== input ======
-const output = document.getElementById("output");
-
 //======== telecom logos ========
-const mtnlogo = document.getElementsByClassName("mtn");
-const airtellogo = document.getElementsByClassName("airtel");
-const glologo = document.getElementsByClassName("glo");
-const ninemobile = document.getElementsByClassName("mobile");
+let mtnLogo = document.getElementsByClassName("mtn");
+let airtellogo = document.getElementsByClassName("airtel");
+let glologo = document.getElementsByClassName("glo");
+let ninemobile = document.getElementsByClassName("mobile");
+
+//====== input ======
+
+// let getNumType = (number) => {
+//   if (number.match(mtn.map())) {
+//     let showLogo = (mtnLogo.style.display = "contents");
+//     return showLogo;
+//   }
+// };
 
 function startApp() {
-  mtn.forEach((string) => {});
-  // Your entire app should not necessarily be coded inside this
-  // single function (though there's no penalty for that),
-  // so create and use/call additional functions from here
-  // pls remove the below and make some magic in here!
-}
+  function setInputText(number) {
+    let typeField = document.getElementById("output");
+    typeField.innerHTML = getMobileType(number);
+  }
 
+  function getMobileType(number) {
+    for (let i = 0; i < mtn.length; i++) {
+      let mtnArray = mtn[i];
+    }
+    console.log(mtnArray);
+  }
+  getMobileType();
+}
 // ======= DO NOT EDIT ============== //
 export default startApp;
 // ======= EEND DO NOT EDIT ========= //
